7ae4703d8af21414a2b7643a5caa8ce51a6ca557
Maciej Litwiniuk <maciej@litwiniuk.net>
2015-01-05 22:14:50 +0100

    install ffmpeg from ppa

--- recipes/package.rb
+++ recipes/package.rb
@@ -10,3 +10,16 @@ ffmpeg_packages.each do |pkg|
     action :upgrade
   end
 end
+
+apt_repository "ffmpeg" do
+  uri "http://ppa.launchpad.net/jon-severinsson/ffmpeg/ubuntu"
+  distribution node["lsb"]["codename"]
+  components ["main"]
+  keyserver "keyserver.ubuntu.com"
+  key "C300EE8C"
+end
+
+apt_package "ffmpeg" do
+  options "--force-yes"
+  :upgrade
+end
