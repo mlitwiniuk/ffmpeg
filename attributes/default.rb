d19c5ca91ec243cf11322a3423882e993ce2804f
Maciej Litwiniuk <maciej@litwiniuk.net>
2015-01-05 22:14:08 +0100

    update default.rb - package as default install method

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2014, Escape Studios
 #
 
-default['ffmpeg']['install_method'] = :source
+default['ffmpeg']['install_method'] = :package
 default['ffmpeg']['prefix'] = '/usr/local'
 default['ffmpeg']['git_repository'] = 'git://git.videolan.org/ffmpeg.git'
 default['ffmpeg']['compile_flags'] = [
