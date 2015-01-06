30ac769cc66ba52240c694e2eab84935419662ea
Maciej Litwiniuk <maciej@litwiniuk.net>
2015-01-06 14:07:42 +0100

    Get ffmpeg from newer PPA

--- recipes/package.rb
+++ recipes/package.rb
@@ -5,19 +5,13 @@
 # Copyright 2014, Escape Studios
 #
 
-#ffmpeg_packages.each do |pkg|
-#  package pkg do
-#    action :upgrade
-#  end
-#end
-
 apt_repository "ffmpeg" do
-  uri "http://ppa.launchpad.net/jon-severinsson/ffmpeg/ubuntu"
+  uri "http://ppa.launchpad.net/mc3man/trusty-media/ubuntu"
   distribution node["lsb"]["codename"]
   components ["main"]
   keyserver "keyserver.ubuntu.com"
-  key "C300EE8C"
-  notifies :run, "execute[apt-get update]", :immediately
+  key "ED8E640A"
+  # notifies :run, "execute[apt-get update]", :immediately
 end
 
 apt_package "ffmpeg" do
